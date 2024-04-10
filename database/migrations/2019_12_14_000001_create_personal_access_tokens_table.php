9a0e86ebae169d4ce15c51babc42b50541333f24
Carole FOURNY <carole.fourny@Digital-Grenoble.local>
2024-04-10 16:12:03 +0200

    login et logout ok

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->uuid('tokenable_id'); // Corrected from 'tokable_id' to 'tokenable_id'
+            $table->string('tokenable_type');
             $table->string('name');
-            $table->string('token', 64)->unique();
+            $table->string('token')->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
