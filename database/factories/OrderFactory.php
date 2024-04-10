a1c08feb0c4a8ede459f68b5dc19c197e831c920
Carole FOURNY <carole.fourny@Digital-Grenoble.local>
2024-04-10 16:50:08 +0200

    commentaire

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -7,11 +7,31 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Order;
 use Faker\Generator as Faker;
 
-
+/**
+ * Class OrderFactory
+ *
+ * This class is responsible for creating fake instances of the Order model for testing purposes.
+ * It extends Laravel's Factory class.
+ */
 class OrderFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = Order::class;
 
+    /**
+     * Define the model's default state.
+     *
+     * This method returns an array of fake data that matches the Order model's attributes.
+     * The 'command_number' attribute is a UUID.
+     * The 'user_id' attribute is the ID of the first user in the database.
+     * The 'created_at' and 'updated_at' attributes are dates between 2 years ago and now.
+     *
+     * @return array
+     */
     public function definition(): array
     {
         $user = User::all()->first();
--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -9,16 +9,39 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
 
 /**
+ * Class StoreFactory
+ *
+ * This class is responsible for creating fake instances of the Store model for testing purposes.
+ * It extends Laravel's Factory class.
+ */
+/**
  * @property $uuid
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Store>
  */
 class StoreFactory extends Factory
 {
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = Store::class;
     protected $company;
     protected $paragraph;
 
 
+    /**
+     * Define the model's default state.
+     *
+     * This method returns an array of fake data that matches the Store model's attributes.
+     * The 'user_id' attribute is a random user ID from the database.
+     * The 'name' attribute is a fake company name.
+     * The 'theme' attribute is a fake word.
+     * The 'biography' attribute is a fake paragraph.
+     *
+     * @return array
+     */
     public function definition() : array
     {
         $userIds = User::pluck('id')->toArray();
